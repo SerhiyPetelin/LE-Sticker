ad25d8a03a40b2c8728d17d668cf39cdcfe6d175
SerhiyPetelin <serhiypetelin@gmail.com>
2023-11-22 23:53:54 +0200

    Add images and structurized them. Start products objects list.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Bootstrap ped progect
-## It's Stiker's store website inspired by Viacheslav Koldovsky.
+## It's Stiker's store website inspired by Viacheslav Koldovsky AKA Programming Mentor.
---
+++ api/products.js
@@ -0,0 +1,20 @@
+/* 
+in stock
+out of stock
+*/
+const products = [
+    {
+        id: 1,
+        title: "Sticker pack #1",
+        category: "stickers",
+        price: 15,
+        banner: "./img/banners/pack-1-banner.png",
+        'slide-1': "./img/product-slides/slide-1-pack-1.png",
+        'slide-2': "./img/product-slides/slide-2-pack-1.png",
+        'slide-3': "./img/product-slides/slide-3-pack-1.png",
+        description: "22 screen printed stickers",
+        status: "in stock",
+        "new arrival": false,
+        "best seller": false
+    },
+];
