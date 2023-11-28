2ee2eb2ee4282c205a55058288d1fe18751cf3ea
SerhiyPetelin <serhiypetelin@gmail.com>
2023-11-28 23:02:34 +0200

    Styling slick slider.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,7 @@
-// $('.heroes__slider-img').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,    
-//   asNavFor: '.heroes__slider-text',
-//   prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-//   nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
-//   responsive: [
-//       {
-//         breakpoint: 769,
-//         settings: {
-//           arrows: false,
-//         }
-//       },
-//   ]
-// });
-$('.slide').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+
+  });
 });
